a6a738d51a5515e8eaa5abd7f8c4217d143bb9e1
greenmac <alwaysmac@msn.com>
2019-04-01 16:57:31 +0800

    validation1

--- 02-general-use/2.2-general-pattern.py
+++ 02-general-use/2.2-general-pattern.py
@@ -14,8 +14,10 @@ iris_y = iris.target
 # 把数据集分为训练集和测试集，其中 test_size=0.3，即测试集占总数据的 30%
 X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.3)
 # print(y_train)
+print(iris_X)
+
 
 knn = KNeighborsClassifier()
 knn.fit(X_train, y_train) # fit函式, 丟進去就幫我們訓練
-print(knn.predict(X_test))
-print(y_test)
+# print(knn.predict(X_test))
+# print(y_test)
